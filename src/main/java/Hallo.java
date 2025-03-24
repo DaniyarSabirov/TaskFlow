a0d27fba0aec4e7cbb991c6c6970c4858e2da23f
akareset <icaproz@gmail.com>
2025-03-24 16:53:47 +0100

    Hallo

--- src/main/java/Hallo.java
+++ src/main/java/Hallo.java
@@ -2,5 +2,7 @@ public class Hallo {
     public static void main(String[] args) {
 
         System.out.println("Hallo world");
+        System.out.println("JHHDKKjk");
+        System.out.println("JHHDKKjk");
     }
 }
